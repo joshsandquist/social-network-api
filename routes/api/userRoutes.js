bc55f11b45fbb03ea1d0bd206d02e4f8ef3db52c
Josh Sandquist <joshdsand@gmail.com>
2023-06-07 17:35:33 -0700

    fixed a typo in userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,11 +13,11 @@ const {
 
 //user routes using api/users endpoint
 router.get('/', getAllUsers);
-router.get('/userId', getUserById);
+router.get('/:userId', getUserById);
 router.post('/', createUser);
 router.put('/:userId', updateUser);
 router.delete('/:userId', deleteUser);
-router.post('userId/friends/:friendId', addFriend);
+router.post('/:userId/friends/:friendId', addFriend);
 router.delete('/:userId/friends/:friendId', removeFriend);
 
 module.exports = router;
